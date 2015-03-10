019e7a86863cca7709f86f089a8a002d9a5c7434
Thong Vu <thong@appswiz.com>
2015-03-11 08:44:19 +1100

    Initial Commit

---
+++ PacteraTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PacteraTestApp
+//
+//  Created by AppsWiz on 11/03/2015.
+//  Copyright (c) 2015 AppsWiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PacteraTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PacteraTestApp
+//
+//  Created by AppsWiz on 11/03/2015.
+//  Copyright (c) 2015 AppsWiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
